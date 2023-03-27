2b294f664cddfb8b245b2fb087f2e69db7b3dad8
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-03-27 13:05:20 +0200

    Use fetch to load photographers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,8 @@
 async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes
-  // de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  const photographers = [
-    {
-      "name": "Mimi Keel",
-			"id": 243,
-			"city": "London",
-			"country": "UK",
-			"tagline": "Voir le beau dans le quotidien",
-			"price": 400,
-			"portrait": "MimiKeel.jpg"
-    },
-    {
-      "name": "Ellie-Rose Wilkens",
-			"id": 930,
-			"city": "Paris",
-			"country": "France",
-			"tagline": "Capturer des compositions complexes",
-			"price": 250,
-			"portrait": "EllieRoseWilkens.jpg"
-    },
-  ];
-  // et bien retourner le tableau photographers seulement une fois récupéré
-  return ({
-    photographers: [...photographers, ...photographers, ...photographers],
-  });
+  const response = await fetch('/data/photographers.json');
+  const photographers = await response.json();
+
+  return photographers;
 }
 
 async function displayData(photographers) {

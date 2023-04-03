5a49c02f6ec146ddb98ada3cf19a74b9171e53f6
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-04-03 10:51:20 +0200

    Implement photographer card link at the correct spot

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -13,10 +13,10 @@ export default class PhotographerCard {
               src="${this.photographer.getPortrait()}" 
               class="photographer-card__image">
             <h2 class="photographer-card__name">${this.photographer.getName()}</h2>
-            <p class="photographer-card__location">${this.photographer.getCity()}, ${this.photographer.getCountry()}</p>
-            <p class="photographer-card__tagline">${this.photographer.getTagline()}</p>
-            <p class="photographer-card__price">${this.photographer.getPrice()}€/jour</p>
           </a>
+          <p class="photographer-card__location">${this.photographer.getCity()}, ${this.photographer.getCountry()}</p>
+          <p class="photographer-card__tagline">${this.photographer.getTagline()}</p>
+          <p class="photographer-card__price">${this.photographer.getPrice()}€/jour</p>
         </article>
       </li>
     `;

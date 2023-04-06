6bfe8fe82b836f2e80a461730133e71c9ed8d243
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-04-06 18:03:44 +0200

    fluid zoom on info aside

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -22,9 +22,9 @@ async function loadPhotographer() {
 function displayData(photographer) {
   photographer.updatePageTitle();
   photographer.renderHeader();
-  // photographer.renderInfoSection();
+  //photographer.renderInfoSection();
   // photographer.renderMediasSortSelect();
-  // photographer.renderMedias();
+  photographer.renderMedias();
 }
 
 async function main() {

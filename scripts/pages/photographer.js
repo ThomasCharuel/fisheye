330c31c48e77aabf74b2a150721a55d92ee7b210
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-04-12 12:01:10 +0200

    Comment on photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,14 +3,18 @@ import MediasApi from "../api/MediasApi.js";
 import createPhotographer from "../factories/photographer.js";
 import createMedia from "../factories/media.js";
 
+// Load Photographer data and medias associated
 async function loadPhotographer() {
+  // Get photographer ID based on URL
   const urlParams = new URLSearchParams(window.location.search);
   const photographerId = Number(urlParams.get('id'));
 
+  // Get photographer Data
   const photographerApi = new PhotographerApi();
   const photographerData = await photographerApi.getPhotographer(photographerId);
   const photographer = createPhotographer(photographerData);
 
+  // Get photographer medias
   const mediasApi = new MediasApi();
   const mediasData = await mediasApi.getMedias(photographerId);
   const medias = mediasData.map((data) => createMedia(data));
@@ -19,6 +23,7 @@ async function loadPhotographer() {
   return photographer;
 }
 
+// Display photographer page sections
 function displayData(photographer) {
   photographer.updatePageTitle();
   photographer.renderHeader();
